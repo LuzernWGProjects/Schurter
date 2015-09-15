6cc13840eb12128e14877f415e67c1262c6fcc9f
Oddwood <me@marconeuhaus.ch>
2015-09-15 14:42:39 -0700

    Test Commit change marco

--- WebCrawler/src/test2/WebCrawler.java
+++ WebCrawler/src/test2/WebCrawler.java
@@ -4,7 +4,7 @@ public class WebCrawler {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo Schurter");
+		System.out.println("Hallo Schurter222");
 
 	}
 
