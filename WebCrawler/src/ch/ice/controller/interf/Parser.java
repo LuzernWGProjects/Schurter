a9369b59469a5a8789db298ed3ab79ec92b0c248
Oddwood <me@marconeuhaus.ch>
2015-09-16 18:23:02 -0700

    Added Parser Interface

--- WebCrawler/src/ch/ice/controller/interf/Parser.java
+++ WebCrawler/src/ch/ice/controller/interf/Parser.java
@@ -1,12 +1,17 @@
-/**
- * 
- */
+
 package ch.ice.controller.interf;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
- * @author Oliver
+ * 
+ * @author mneuhaus
  *
  */
 public interface Parser {
-
+	
+	// input can either be a Excel File or a CSV file
+	public void readFile(File file) throws IOException;
+		
 }
