fd621a75a2fe694dbf670fd333c88889f349f39c
ofaust90 <oli_fau@hotmail.com>
2015-09-17 21:42:30 +0200

    change in startsearch

--- WebCrawler/src/ch/ice/controller/MainController.java
+++ WebCrawler/src/ch/ice/controller/MainController.java
@@ -32,26 +32,34 @@ public class MainController {
 		
 	}
 	
-	public static void startSearch(){
-		System.out.println("start test bing");
 	
-		String query = "emmi ";
-		try {
+	public static String startSearch(String query){
+		System.out.println("start test bing");
+		String url = null;
 		
-			JSONArray results = BingSearchEngine.Search(query);
-	 
-			final int resultsLength = results.length();
+		try {
+			
+		 JSONArray results = BingSearchEngine.Search(query);
+	/*	 
+	     final int resultsLength = results.length();
 
-			for (int i = 0; i < resultsLength; i++) {
-				JSONObject   aResult = results.getJSONObject(i);
-				System.out.println(aResult.get("Url"));
-    
-			}
+		   for (int i = 0; i < resultsLength; i++) {
+	    	   JSONObject   aResult = results.getJSONObject(i);
+	        System.out.println(aResult.get("Url"));
+	    }
+	      */ 
+	        JSONObject   JsonObj = results.getJSONObject(0); 
+	        
+	         url = (String) JsonObj.get("Url");
+	      	
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
+		  return url;
+		}
+	
 	
 	// Test purpose
 	public static void startExcelParser(){
