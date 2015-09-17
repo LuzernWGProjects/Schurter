e8f7f5258d60e17b03893f6633ce7f2b1ed715fd
Oddwood <me@marconeuhaus.ch>
2015-09-17 12:32:06 -0700

    Added fields to Customer Model +getter and setter

--- WebCrawler/src/ch/ice/model/Customer.java
+++ WebCrawler/src/ch/ice/model/Customer.java
@@ -1,12 +1,63 @@
-/**
- * 
- */
 package ch.ice.model;
 
 /**
- * @author Oliver
+ * @author Marco
  *
  */
 public class Customer {
-
+	
+	private String customerID;
+	private String customerName;
+	private String countryCode;
+	private String countryName;
+	private String zipCode;
+	private String customerNameShort;
+	
+	public String getCustomerID() {
+		return customerID;
+	}
+	
+	public void setCustomerID(String customerID) {
+		this.customerID = customerID;
+	}
+	
+	public String getCustomerName() {
+		return customerName;
+	}
+	
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+	public String getCountryCode() {
+		return countryCode;
+	}
+	
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+	
+	public String getCountryName() {
+		return countryName;
+	}
+	
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+	
+	public String getZipCode() {
+		return zipCode;
+	}
+	
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+	public String getCustomerNameShort() {
+		return customerNameShort;
+	}
+	
+	public void setCustomerNameShort(String customerNameShort) {
+		this.customerNameShort = customerNameShort;
+	}
 }
