dc40894c7ebe34b3ef524d07e2adc9bb2e9ef249
Oddwood <me@marconeuhaus.ch>
2015-09-17 12:26:44 -0700

    Added test method for excel parser

--- WebCrawler/src/ch/ice/controller/MainController.java
+++ WebCrawler/src/ch/ice/controller/MainController.java
@@ -3,8 +3,14 @@
  */
 package ch.ice.controller;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import ch.ice.controller.parser.ExcelParser;
+import ch.ice.controller.interf.Parser;
 
 /**
  * @author Oliver
@@ -16,7 +22,8 @@ public class MainController {
 	public static void startMainController()
 	{
 		
-		startSearch();
+		//startSearch();
+		startExcelParser();
 	}
 	
 	
@@ -26,28 +33,37 @@ public class MainController {
 	}
 	
 	public static void startSearch(){
-	System.out.println("start test bing");
+		System.out.println("start test bing");
 	
-	String query = "emmi ";
-	try {
+		String query = "emmi ";
+		try {
 		
-	 JSONArray results = BingSearchEngine.Search(query);
+			JSONArray results = BingSearchEngine.Search(query);
 	 
-     final int resultsLength = results.length();
+			final int resultsLength = results.length();
 
-	   for (int i = 0; i < resultsLength; i++) {
-    	   JSONObject   aResult = results.getJSONObject(i);
-        System.out.println(aResult.get("Url"));
+			for (int i = 0; i < resultsLength; i++) {
+				JSONObject   aResult = results.getJSONObject(i);
+				System.out.println(aResult.get("Url"));
     
-    }
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
+	// Test purpose
+	public static void startExcelParser(){
+		Parser excelParser = new ExcelParser();
 		
-	} catch (Exception e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+		try {
+			excelParser.readFile(new File("posTest.xlsx"));
+		} catch (IOException e) {
+			System.out.println("File not found");
+			e.printStackTrace();
+		}
 	}
-	}
-	
 	
 	public static void startWriter()
 	{
