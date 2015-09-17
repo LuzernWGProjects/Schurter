10761b67867f0c4efc677a0e5065f1e39516223a
ofaust90 <oli_fau@hotmail.com>
2015-09-17 03:20:08 +0200

    added Bing Search Engine class

--- WebCrawler/src/ch/ice/Main.java
+++ WebCrawler/src/ch/ice/Main.java
@@ -3,6 +3,8 @@
  */
 package ch.ice;
 
+import ch.ice.controller.BingSearchEngine;
+
 /**
  * @author Oliver
  *
@@ -14,6 +16,17 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("start test bing");
+		
+		String query = "schurter luzern";
+		try {
+			BingSearchEngine.Search(query);
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
