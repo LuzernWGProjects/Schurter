865934622f539e114225d05f1007c50216198660
Pascal Engel <pascal.engel@stud.hslu.ch>
2015-09-19 01:41:08 +0200

    ExcelWriter added function

--- WebCrawler/src/ch/ice/controller/ExcelWriter.java
+++ WebCrawler/src/ch/ice/controller/ExcelWriter.java
@@ -3,16 +3,34 @@
  */
 package ch.ice.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
 import ch.ice.controller.interf.Writer;
 
 /**
  * @author Oliver
  *
  */
+
 public class ExcelWriter implements Writer {
 	
+	public ExcelWriter(){
 	HSSFWorkbook workbook = new HSSFWorkbook();
 	HSSFSheet sheet = workbook.createSheet("Sample sheet");
+	}
+	
 	
 	Map<String, Object[]> data = new HashMap<String, Object[]>();
 	data.put("1", new Object[] {"Emp No.", "Name", "Salary"});
@@ -46,10 +64,10 @@ public class ExcelWriter implements Writer {
 		out.close();
 		System.out.println("Excel written successfully..");
 		
-	} catch (FileNotFoundException e) {
-		e.printStackTrace();
-	} catch (IOException e) {
-		e.printStackTrace();
+	} catch (FileNotFoundException ea) {
+		ea.printStackTrace();
+	} catch (IOException er) {
+		er.printStackTrace();
 	}
 
-}
+}}
