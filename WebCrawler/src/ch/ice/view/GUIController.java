67b100acf976b9e4b640a9de7ad6917d6dc593d9
GruettA <pascal.gruetter.za@gmail.com>
2015-09-23 14:54:29 -0700

    GUI Changes

--- WebCrawler/src/ch/ice/view/GUIController.java
+++ WebCrawler/src/ch/ice/view/GUIController.java
@@ -1,5 +1,6 @@
 package ch.ice.view;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
@@ -19,6 +20,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -46,10 +48,16 @@ public class GUIController implements Initializable {
 	private Button okMetaButton;
 	@FXML
 	private Label metaTagsList;
+	@FXML
+	private TextField pathTextField;
+	@FXML
+	private Button changeDirectory;
 
 	public static PropertiesConfiguration config;
 
 	public static List<String> metaTagElements;
+
+	public static String path;
 
 	public static void getProperties(Label label) {
 		try {
@@ -65,11 +73,38 @@ public class GUIController implements Initializable {
 
 	}
 
+	public static void getSaveProperties() {
+		try {
+			config = new PropertiesConfiguration("conf/app.properties");
+			path = config.getString(("writer.file.path"));
+
+		} catch (ConfigurationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+	}
+
+	public static void setSaveProperties(String chosenPath) {
+		try {
+			config = new PropertiesConfiguration("conf/app.properties");
+			config.setProperty("writer.file.path", chosenPath);
+
+		} catch (ConfigurationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+	}
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
 		getProperties(metaTagsList);
 		FileChooser filechooser = new FileChooser();
+		DirectoryChooser directoryChooser = new DirectoryChooser();
+		getSaveProperties();
+		pathTextField.setText(path);
 
 		selectFileButton.setOnAction(new EventHandler<ActionEvent>() {
 
@@ -78,6 +113,11 @@ public class GUIController implements Initializable {
 
 				Stage stage = new Stage();
 				try {
+					filechooser.getExtensionFilters().addAll(
+							new FileChooser.ExtensionFilter(
+									"Excel-File (*.xls)", "*.xls"),
+							new FileChooser.ExtensionFilter(
+									"Excel-File (*.xlsx)", "*.xlsx"));
 					MainController.file = filechooser.showOpenDialog(stage);
 					if (MainController.file != null) {
 						fileTextField.setText(MainController.file
@@ -92,6 +132,31 @@ public class GUIController implements Initializable {
 			}
 		});
 
+		changeDirectory.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				Stage stage = new Stage();
+				try {
+
+					File pathFile = directoryChooser.showDialog(stage);
+					if (pathFile != null) {
+						setSaveProperties(pathFile.getAbsolutePath());
+						config.save();
+						getSaveProperties();
+						pathTextField.setText(path);
+
+					}
+				} catch (NullPointerException | ConfigurationException e) {
+
+					System.out.println("No Path selected");
+					pathTextField.setText("");
+				}
+			}
+
+		});
+
 		startSearchButton.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
@@ -100,6 +165,13 @@ public class GUIController implements Initializable {
 				MainController main = new MainController();
 
 				main.startMainController();
+				// int i = 0;
+				// int rows = main.getsize();
+				// while (i < rows) {
+				// Thread.sleep(250);
+				// i = main.getcurrent();
+				// searchProgressBar.setProgress(i);
+				// }
 
 			}
 		});
