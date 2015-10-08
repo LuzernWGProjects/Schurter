d2c599c11d1fc1225440bc2d0ecf52970f020eb0
ofaust90 <oli_fau@hotmail.com>
2015-10-08 07:51:08 +0200

    enhanced search logic

--- WebCrawler/src/ch/ice/controller/web/ResultAnalyzer.java
+++ WebCrawler/src/ch/ice/controller/web/ResultAnalyzer.java
@@ -30,6 +30,7 @@ public class ResultAnalyzer {
 	static URI uri;
 	private static final Logger logger = LogManager
 			.getLogger(MainController.class.getName());
+	static public double[] candidatesArray ;
 	
 	/**
 	 * Method to analyze which received result suits the best
@@ -42,63 +43,156 @@ public class ResultAnalyzer {
 	public static JSONObject analyze(JSONArray results, List<String> parameters)
 	{
 		System.out.println("Laenge: "+results.length()+";");
+		
+		//initlayze candiatesArray for decision making
+		candidatesArray = new double[results.length()];
+		for (int y = 0; y < candidatesArray.length; y++) {
+			candidatesArray[y] = 0;
+		}
+		
 		//Go thru each received result of the search request
 		for (int i = 0; i < results.length(); i++) {
 			
 			//create a new temporary JSONObject for each result
 			JSONObject singleResult = results.getJSONObject(i);
 			results.getJSONObject(i).put("Unsure", false);
+		
 			
 			//extract the url, title and the desc
 			String url = (String) singleResult.get("Url");
 			String title = (String) singleResult.get("Title");
 			String desc = (String) singleResult.get("Description");
 			
-			
+			/* ******************************************
+			 * Pre-processing for further analysis 
+			 *********************************************/
 			try {
 				uri = new URI(url);
 			} catch (URISyntaxException e) {
 				e.printStackTrace();
 			}
 			String host = uri.getHost().replaceFirst("^(http://|http://www\\.|www\\.)","");
-			
+
 			//store name of company in a variable to work with later on
 			String companyName = (String) parameters.get(0);
+			char[] fullCompanyNameCharSequence = parameters.get(0).toCharArray();
+			
+			//spilt single words of the name
+			String[] singleWords = companyName.split("\\s+");
+			for (int j = 0; j < singleWords.length; j++) {
+				singleWords[j] = singleWords[j].replaceAll("[^\\w]", "");
+			}
+			
+			//split url in single words
+			String strippedURL =	host.replace("-", " ").replaceAll("[^\\w]", " ");
+			String[] singleURLWords = strippedURL.split("\\s+");
+			
 			//create acronym of company name
 			String acroOfCompanyName = createAcronym(companyName);
 		
-			// if the url is on the blacklist, remove this element form the list
-			if(isBlacklist(host))
-				{
-					System.out.println("remove because blacklist");
-					results.remove(i);
-					
-					logger.info("ResultAnalyzer: Blacklisted");
-					//recursive call of the same function
-					analyze(results, parameters);
-				}
-		
-			// if the url contains the company name return this as the best result
-			if(host.contains((CharSequence) parameters.get(0)))
-					{
+			/* *******************************************
+			 * Start the basics("killer criteria") checks
+			 *********************************************/
+			
+			// if the url contains the full company name return this as the best result
+			if(host.contains(companyName)){
 				System.out.println("----------- NAMEN in URL!" +url +"diese url enthält den namen: "+ parameters.get(0));
 				logger.info("ResultAnalyzer: Contains full name");
 				return singleResult;
-					}
-			// else if the url contains a acronym of the name return this as the best result
-			else if(host.contains((CharSequence) acroOfCompanyName)){
-			
-				System.out.println("-----------ABK in URL: Diese Url enthält die abk: " + acroOfCompanyName);
-				logger.info("ResultAnalyzer: Contains URL");
-			
-				return singleResult;
 			}
+						
+			// if the url is not on the blacklist, do further checks; otherwise this result will have a 0.0 score
+			if(!isBlacklist(host)){
+						//	System.out.println("remove because blacklist");
+						//	results.getJSONObject(i).put("Blacklisted", true);
+					//		logger.info("ResultAnalyzer: Blacklisted");
+							//recursive call of the same function
+					//		analyze(results, parameters);
+					
+				
+			
+				
+				/* *******************************************
+				 * Do further checks
+				 *********************************************/
+				//is first word in company name equal than first in url?
+				if(singleWords[0].equals(singleURLWords[0]))
+				{
+					candidatesArray[i] = candidatesArray[i]+(1*0.5);
+					System.out.println(candidatesArray[i]);
+				
+					
+				}
+				//does the url contains the first word of the company name?
+				if(host.contains(singleWords[0]))
+				{
+					candidatesArray[i] = candidatesArray[i]+(1*0.2);
+					System.out.println(candidatesArray[i]);
+				}
+				//if the company name contains more than 1 word...
+				if(singleWords.length > 1)
+				{	//does the url any other word of the name?
+					for (int j = 0; j < singleURLWords.length -1; j++) {
+						if(host.contains(singleWords[j]))
+						{
+							candidatesArray[i] = candidatesArray[i]+(1*0.1);
+							System.out.println(candidatesArray[i]);
+						}
+					}
+				}	
+				// does the url contains the acronym of the company?
+				if(host.equals((CharSequence) acroOfCompanyName)){
+				
+					candidatesArray[i] = candidatesArray[i]+(1*0.5);
+					System.out.println("-----------ABK in URL: Diese Url enthält die abk: " + acroOfCompanyName);
+					logger.info("ResultAnalyzer: Contains URL");
+				
+					System.out.println(candidatesArray[i]);
+				}
+				
+				
+				//check if unsure (the critical value is 0.5 since if is less than 0.5, the first word is not contained in the url)
+				if(candidatesArray[i] < 0.5)
+				{
+					results.getJSONObject(i).put("Unsure", true);
+				}
+			
+			}else{
+				logger.info("ResultAnalyzer: Blacklisted");
+				System.out.println("blacklisted");
+				results.getJSONObject(i).put("Unsure", true);
+			}
+			System.out.println(url+" Score: "+ candidatesArray[i]);
+		}// ************END OF FOR
 		
+
+		//determine the best result
+		double max = candidatesArray[0];
+		
+		for (int counter = 1; counter < candidatesArray.length; counter++)
+		{
+		     if (candidatesArray[counter] > max)
+		     {
+		      max = candidatesArray[counter];
+		     }
 		}
+		// return the object which is the best score
+		for (int l = 0; l < candidatesArray.length; l++) {
+			if(candidatesArray[l]== max)
+			{	
+				System.out.println("highest Score:" +candidatesArray[l]);
+				return results.getJSONObject(l);
+			}
+		}
+		
+	
+		
+		
 		//if neither the name nor the acronym of the name is contained in the url, return the first result
-		System.out.println("--------- Nicht gefunden nimm erstes");
-		logger.info("ResultAnalyzer: unsure; take first");
-		return results.getJSONObject(0).put("Unsure", true);
+	//	System.out.println("--------- Nicht gefunden nimm erstes");
+	//	logger.info("ResultAnalyzer: unsure; take first");
+	//	return results.getJSONObject(0).put("Unsure", true);
+		return null;
 		//return results.getJSONObject(0);
 	}
 	
@@ -116,7 +210,7 @@ public class ResultAnalyzer {
 		{
 			acro+=word.charAt(0);
 		}
-		System.out.println(acro);
+	
 		
 		
 		return acro;
