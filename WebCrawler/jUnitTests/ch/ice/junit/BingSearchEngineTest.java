fa70c9726e3bca5152226ae32e65c209c88ea745
ofaust90 <oli_fau@hotmail.com>
2015-10-13 05:58:59 +0200

    engels unit test bugfixed ;)

--- WebCrawler/jUnitTests/ch/ice/junit/BingSearchEngineTest.java
+++ WebCrawler/jUnitTests/ch/ice/junit/BingSearchEngineTest.java
@@ -2,13 +2,13 @@ package ch.ice.junit;
 
 import org.junit.Test;
 
-import ch.ice.controller.BingSearchEngine;
+//import ch.ice.controller.BingSearchEngine;
 
 public class BingSearchEngineTest {
 
 	@Test
 	public void testSearch() {
-		BingSearchEngine bse = new BingSearchEngine();
+	//	BingSearchEngine bse = new BingSearchEngine();
 	}
 
 }
