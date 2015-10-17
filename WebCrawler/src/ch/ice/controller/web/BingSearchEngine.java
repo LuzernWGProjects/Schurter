52ad1639b18973fd9ed7dd51a10cb47bfc33dc82
ofaust90 <oli_fau@hotmail.com>
2015-10-18 00:08:05 +0200

    fixing merge problems

--- WebCrawler/src/ch/ice/controller/web/BingSearchEngine.java
+++ WebCrawler/src/ch/ice/controller/web/BingSearchEngine.java
@@ -27,20 +27,9 @@ import ch.ice.utils.XMLParser;
 
 
 public class BingSearchEngine implements SearchEngine {
-<<<<<<< HEAD
-	
-	private static Map<String, String> country2Market = XMLParser.getMarket();
-	/**
-	 * Search for an URL on Bing with certain params
-	 * 
-	 * @param String - The requested Query for lookup
-	 * @param int - Limit the retrieved search results
-	 * @param String - Limit Search for a country e.g bing.ch, bing.co.uk and so on
-	 * @return JSONArray with all searchresults on Bing
-	 */
-=======
 
->>>>>>> branch 'master' of https://github.com/LuzernWGProjects/Schurter.git
+	private static Map<String, String> country2Market = XMLParser.getMarket();
+
 	@Override
 	public JSONArray search(String requestedQuery, int limitSearchResults, String countryCode)  throws IOException, NoUrlFoundException {
 
@@ -51,68 +40,55 @@ public class BingSearchEngine implements SearchEngine {
 		/*
 		 * Load Configuration File
 		 */
-<<<<<<< HEAD
-		try {
-			config = new PropertiesConfiguration("conf/app.properties");
-			
-			accountKey = config.getString("searchEngine.bing.accountKey");
-			bingUrlPattern = config.getString("searchEngine.bing.pattern");
-			
-		} catch (ConfigurationException e) {
-			System.out.println(e.getLocalizedMessage());
-			e.printStackTrace();
-		}
-=======
+		config = Config.PROPERTIES;
+
 		accountKey = config.getString("searchEngine.bing.accountKey");
 		bingUrlPattern = config.getString("searchEngine.bing.pattern");
->>>>>>> branch 'master' of https://github.com/LuzernWGProjects/Schurter.git
 
-<<<<<<< HEAD
+		accountKey = config.getString("searchEngine.bing.accountKey");
+		bingUrlPattern = config.getString("searchEngine.bing.pattern");
+
 		String MarketCode= null;;
-		
+
 		// country code and google host mapping
-	
+
 		String quotes = "'";
 		String quotesEnc = URLEncoder.encode(quotes, Charset.defaultCharset().name());
 
-		
+
 		String reqQueryEnc = "'"+requestedQuery+"'";
-        String query = URLEncoder.encode(reqQueryEnc, Charset.defaultCharset().name());
-		
+		String query = URLEncoder.encode(reqQueryEnc, Charset.defaultCharset().name());
+
 		String MarketURL ="";
 		//	if(country2Market.get(countryCode.toLowerCase()).isEmpty() || country2Market.get(countryCode.toLowerCase()) == null){
 		if(country2Market.get(countryCode.toLowerCase()) == null ){
 			MarketCode = "";
 		} else {
-			
+
 			MarketCode = URLEncoder.encode(country2Market.get(countryCode),Charset.defaultCharset().name());
 			MarketURL = "&Market="+quotesEnc+MarketCode+quotesEnc;
 
 		}
-    	
-		
-    	// Bing Constants
-       
-        // if search results limit is smaller then 1, set to 1
-        if(limitSearchResults < 1) limitSearchResults = 1;
-  
-        String bingUrl = bingUrlPattern+query+"&$format=JSON&$top="+limitSearchResults+MarketURL;
-    
-        String accountKeyEnc = Base64.getEncoder().encodeToString((accountKey + ":" + accountKey).getBytes());
-=======
 
->>>>>>> branch 'master' of https://github.com/LuzernWGProjects/Schurter.git
 
 		// Bing Constants
-
-		String query = URLEncoder.encode(requestedQuery, Charset.defaultCharset().name());
 
 		// if search results limit is smaller then 1, set to 1
 		if(limitSearchResults < 1) limitSearchResults = 1;
 
-		String bingUrl = String.format(bingUrlPattern+"&$top="+limitSearchResults, query);
+		String bingUrl = bingUrlPattern+query+"&$format=JSON&$top="+limitSearchResults+MarketURL;
 
 		String accountKeyEnc = Base64.getEncoder().encodeToString((accountKey + ":" + accountKey).getBytes());
+		// Bing Constants
+
+//		String query = URLEncoder.encode(requestedQuery, Charset.defaultCharset().name());
+//
+//		// if search results limit is smaller then 1, set to 1
+//		if(limitSearchResults < 1) limitSearchResults = 1;
+//
+//		String bingUrl = String.format(bingUrlPattern+"&$top="+limitSearchResults, query);
+//
+//		String accountKeyEnc = Base64.getEncoder().encodeToString((accountKey + ":" + accountKey).getBytes());
 
 		final URL url = new URL(bingUrl);
 		final URLConnection connection = url.openConnection();
@@ -171,7 +147,7 @@ public class BingSearchEngine implements SearchEngine {
 			return bingResults;
 		}
 	}
-	
+
 	@Override
 	public JSONArray search(String requestedQuery, int limitSearchResults) throws IOException, NoUrlFoundException {
 		return search(requestedQuery, limitSearchResults, "us");
